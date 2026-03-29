440f84315cb61dacbfc1f99ba6053e429fccc37a
RaisedAttic6253 <jiri.antl220@gmail.com>
2026-03-29 20:14:42 +0200

    Teď už opravdu poslední úpravy kodu desky.

--- Kod_deska/kod_deska/include/promene.h
+++ Kod_deska/kod_deska/include/promene.h
@@ -42,9 +42,7 @@ inline byte strana_menu = 0;
 
 inline String URL_POHYB = "http://antl.tomascmol.net/uloz_data_pohybu.php";
 inline String URL_TEPLOTA = "http://antl.tomascmol.net/uloz_data_teploty.php";
-inline String URL_CASU_TEPLOTY = "http://antl.tomascmol.net/ziskat_casy_mereni_teploty.php";
 inline String URL_CHYBEJICI_TEPLOTY = "http://antl.tomascmol.net/odeslat_chybejici_data_teploty.php";
-inline String URL_CASU_POHYBU = "http://antl.tomascmol.net/ziskat_casy_mereni_pohybu.php";
 inline String URL_CHYBEJICIHO_POHYBU = "http://antl.tomascmol.net/odeslat_chybejici_data_pohybu.php";
 
 inline const char* SOUBOR_PRVNI_NEUSPESNY_UPLOAD_TEPLOTY = "/cas_prvniho_neuspesneho_uploadu_teploty.txt";
